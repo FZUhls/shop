9ca1938d3258d9f7e9d0f9e6dd752e590ffdfb60
FZUhls <314433307@qq.com>
2021-09-29 13:13:35 +0800

    1、添加shop-admin模块 2、shop-business模块添加druid监控台的配置

---
+++ shop-admin/src/test/java/com/henry/shop/admin/test/ShopAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.shop.admin.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
