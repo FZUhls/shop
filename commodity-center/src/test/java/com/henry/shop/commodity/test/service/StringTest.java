1b2069a95b32924c5c26d64817673ac98d427297
FZUhls <314433307@qq.com>
2021-11-29 17:22:06 +0800

    添加商品方法2.1.1

--- commodity-center/src/test/java/com/henry/shop/commodity/test/service/StringTest.java
+++ commodity-center/src/test/java/com/henry/shop/commodity/test/service/StringTest.java
@@ -1,10 +1,34 @@
 package com.henry.shop.commodity.test.service;
 
+import com.henry.shop.commodity.dto.req.CommodityDto;
+import com.henry.shop.commodity.dto.req.ParamItemDto;
+import com.henry.shop.commodity.dto.req.SkuDto;
+import com.henry.shop.commodity.service.CommodityService;
+import com.henry.shop.common.base.exception.DataNotFoundException;
+import com.henry.shop.common.base.exception.ParamIllegalException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class StringTest {
+import java.util.List;
+
+public class StringTest extends BaseTest{
+    @Autowired
+    CommodityService commodityService;
     @Test
     void test1(){
-
+        CommodityDto commodityDto = new CommodityDto();
+        commodityDto.setBrandId(1L);
+        commodityDto.setName("衬衫");
+        commodityDto.setSkuDtoList(List.of(new SkuDto()));
+        commodityDto.setParamItemDtoList(List.of(new ParamItemDto()));
+        commodityDto.setVariantGroupId(1L);
+        commodityDto.setCategoryId(1L);
+        try {
+            commodityService.createCommodity(commodityDto);
+        } catch (DataNotFoundException e) {
+            e.printStackTrace();
+        } catch (ParamIllegalException e) {
+            e.printStackTrace();
+        }
     }
 }
